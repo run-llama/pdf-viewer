0271c165cfac8c6096ae7520626386a3ddb2269f
Thuc Pham <51660321+thucpn@users.noreply.github.com>
2024-05-13 14:26:29 +0700

    add option bar

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import { Test } from './view/Test.js';
 import { PDFViewer } from './view/PDFViewer.js';
 import MemoizedVirtualizedPDF from './view/VirtualizedPdf.js';
 import { PdfFocusProvider } from './contexts/PdfFocusContext.js';
+import PDFOptionsBar from './view/PdfOptionsBar.js';
 
-export { Test, PDFViewer, MemoizedVirtualizedPDF, PdfFocusProvider };
+export { Test, PDFViewer, MemoizedVirtualizedPDF, PdfFocusProvider, PDFOptionsBar };
--- src/view/PDFViewer.tsx
+++ src/view/PDFViewer.tsx
@@ -2,16 +2,48 @@ import React from 'react';
 import usePDFViewer from '../hooks/usePdfViewer.js';
 import type { PdfDocument } from '../types/document.js';
 import MemoizedVirtualizedPDF from './VirtualizedPdf.js';
+import PDFOptionsBar from './PdfOptionsBar.js';
 
 interface PDFViewerProps {
   file: PdfDocument;
 }
 
 export const PDFViewer: React.FC<PDFViewerProps> = ({ file }) => {
-  const { setCurrentPageNumber, scale, setScaleFit, setNumPages, pdfFocusRef } = usePDFViewer(file);
+  const {
+    scrolledIndex,
+    setCurrentPageNumber,
+    scale,
+    setScaleFit,
+    numPages,
+    setNumPages,
+    handleZoomIn,
+    handleZoomOut,
+    nextPage,
+    prevPage,
+    scaleText,
+    pdfFocusRef,
+    goToPage,
+    setZoomLevel,
+    zoomInEnabled,
+    zoomOutEnabled,
+  } = usePDFViewer(file);
 
   return (
     <div className="relative">
+      <span>abc</span>
+      <PDFOptionsBar
+        scrolledIndex={scrolledIndex}
+        numPages={numPages}
+        scaleText={scaleText}
+        nextPage={nextPage}
+        prevPage={prevPage}
+        handleZoomIn={handleZoomIn}
+        handleZoomOut={handleZoomOut}
+        goToPage={goToPage}
+        setZoomLevel={setZoomLevel}
+        zoomInEnabled={zoomInEnabled}
+        zoomOutEnabled={zoomOutEnabled}
+      />
       <MemoizedVirtualizedPDF
         key={`${file.id}`}
         ref={pdfFocusRef}
